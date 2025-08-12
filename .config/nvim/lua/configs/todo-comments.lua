cf3acb19f2ff1db64db589ddaa977654587236a2
Felix Pries <34555911+Fix007@users.noreply.github.com>
2025-08-12 21:34:35 +0200

    highlight NOTE and INFO

--- .config/nvim/lua/configs/todo-comments.lua
+++ .config/nvim/lua/configs/todo-comments.lua
@@ -10,7 +10,7 @@ local options = {
     HACK = { icon = " ", color = "warning" },
     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
     PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-    -- NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
     -- TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
   },
   merge_keywords = false,
