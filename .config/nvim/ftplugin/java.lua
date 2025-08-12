de5031653490fa75c68e0738cfce93171e87be07
Felix Pries <34555911+Fix007@users.noreply.github.com>
2025-08-12 21:34:06 +0200

    remove unused config

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,30 +1,3 @@
--- local config = {
---   cmd = { vim.fn.expand "~/.local/share/nvim/mason/bin/jdtls" },
---   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", ".jj", "mvnw" }, { upward = true })[1]),
---   init_options = {
---     settings = {
---       java = {
---         implementationsCodeLens = { enabled = true },
---         imports = { -- <- this
---           gradle = {
---             enabled = true,
---             wrapper = {
---               enabled = true,
---               checksums = {
---                 {
---                   sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172",
---                   allowed = true,
---                 },
---               },
---             },
---           },
---         },
---       },
---     },
---   },
--- }
--- require("jdtls").start_or_attach(config)
-
 local home = os.getenv "HOME"
 local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
